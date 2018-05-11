ec426e562ef964776081f4f6dda5ad2e63bb5299
Maxi Dev <maxi.canellas@gmail.com>
2018-05-10 21:44:05 -0300

    [UPDATE] fixes, npm init and flatten script for contracts.

---
+++ truffle.js
@@ -0,0 +1,31 @@
+require('babel-register')({
+  ignore: /node_modules\/(?!zeppelin-solidity)/
+});
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      gas: 0xfffffffff
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8545,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
+    ropsten: {
+      host: "localhost",
+      network_id: 3,
+      port: 8545,
+      gas: 2900000
+    }
+  },
+  mocha: {
+    useColors: true
+  }
+};
