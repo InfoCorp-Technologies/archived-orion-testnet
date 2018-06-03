f7abd88310621c250a5e87a54ad89f3f6e9cacb0
Sebastián Ponti <sponti@cespi.unlp.edu.ar>
2018-06-03 11:39:13 -0300

    Uncomment home bridge deployment lines.

--- poa-network/deploy/deploy.js
+++ poa-network/deploy/deploy.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 
-//const deployHome = require('./src/home');
+const deployHome = require('./src/home');
 const deployForeign = require('./src/foreign');
 
 async function main() {
-//  const homeBridge = await deployHome()
+  const homeBridge = await deployHome()
   const {foreignBridge} = await deployForeign();
   console.log("\nDeployment has been completed.\n\n")
-//  console.log(`[   Home  ] HomeBridge: ${homeBridge.address} at block ${homeBridge.deployedBlockNumber}`)
+  console.log(`[   Home  ] HomeBridge: ${homeBridge.address} at block ${homeBridge.deployedBlockNumber}`)
   console.log(`[ Foreign ] ForeignBridge: ${foreignBridge.address} at block ${foreignBridge.deployedBlockNumber}`)
   fs.writeFileSync('./bridgeDeploymentResults.json', JSON.stringify({
-//    homeBridge: {
-//      ...homeBridge,
-//    },
-      foreignBridge: {
+    homeBridge: {
+      ...homeBridge,
+    },
+    foreignBridge: {
       ...foreignBridge,
     }
   },null,4));
