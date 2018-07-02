3e46ef61c5fb5a9e42a96a40d5ea985ef5895908
NhatTM-InfoCorp <nhattm@infocorp.io>
2018-07-02 17:08:12 +0700

    Update Oraclize

--- contracts/oracle/Oraclize.sol
+++ contracts/oracle/Oraclize.sol
@@ -4,26 +4,30 @@ import "github.com/oraclize/ethereum-api/oraclizeAPI_0.5.sol";
 
 contract Oraclize is usingOraclize {
     
+    struct QueryInfo {
+        address caller;
+        string result;
+    }
+    
     mapping(bytes => string) apiMap;
-    mapping(bytes32 => address) queryMap;
-    mapping(bytes32 => string) resultMap;
+    mapping(bytes32 => QueryInfo) queryMap;
 
     event Query(bytes32 queryid, string url);
     event Result(bytes32 queryid, address user);
 
     constructor() public {
         apiMap["user"] = "http://104.211.59.231/user/";
-        apiMap["attestator"] = "http://104.211.59.231/attestator";
-        apiMap["livestock"] = "http://104.211.59.231/livestock";
+        apiMap["attestator"] = "http://104.211.59.231/attestator/";
+        apiMap["livestock"] = "http://104.211.59.231/livestock/";
         OAR = OraclizeAddrResolverI(address(0xA536fBAdb9658A980cf36f2b6F0ff075612C9BBc));
         oraclize_setCustomGasPrice(1000000000);
     }
 
     function __callback(bytes32 queryid, string result) public {
-        require(queryMap[queryid] != 0x0);
+        require(queryMap[queryid].caller != 0x0);
         require(msg.sender == oraclize_cbAddress());
-        resultMap[queryid] = result;
-        emit Result(queryid, queryMap[queryid]);
+        queryMap[queryid].result = result;
+        emit Result(queryid, queryMap[queryid].caller);
     }
     
     function api(string name) view external returns(string) {
@@ -35,13 +39,13 @@ contract Oraclize is usingOraclize {
         bytes memory interfaces = bytes(name);
         string memory url = strConcat("json(", apiMap[interfaces], walletaddress, ").result");
         bytes32 queryId = oraclize_query("URL", url, gaslimit);
-        queryMap[queryId] = msg.sender;
+        queryMap[queryId].caller = msg.sender;
         emit Query(queryId, url);
     }
     
     function result(bytes32 queryid) view external returns(string) {
-        require(queryMap[queryid] == msg.sender);
-        return resultMap[queryid];
+        require(queryMap[queryid].caller == msg.sender);
+        return queryMap[queryid].result;
     }
     
     function oar() view external returns(address) {
