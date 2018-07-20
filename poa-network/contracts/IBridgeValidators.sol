c717ab94927fedf5c01fac6e6f2a96e7f84cc935
NhatTM-InfoCorp <nhattm@infocorp.io>
2018-07-20 11:03:43 +0700

    Remove poa-network

--- poa-network/contracts/IBridgeValidators.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.23;
-
-interface IBridgeValidators {
-    function isValidator(address _validator) public view returns(bool);
-    function requiredSignatures() public view returns(uint256);
-    function owner() public view returns(address);
-}
