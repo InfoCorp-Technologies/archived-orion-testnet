d2cd90eee022157c907811205a2a085f774eeae6
Sebastián <sebaponti@gmail.com>
2018-10-12 16:11:31 -0300

    [FIX] Url wildcard for mock api query

--- services/mock-api-query/server.js
+++ services/mock-api-query/server.js
@@ -4,17 +4,17 @@ let userResult = require('./userResult.json');
 let attestatorResult = require('./attestatorResult.json');
 let livestockResult = require('./livestockResult.json');
 
-app.get('/user', (req, res) => {
+app.get('/user*', (req, res) => {
     console.log('|> request: user', '\n');
     res.header("Content-Type", "application/json");
     res.send(userResult);
 });
-app.get('/attestator', (req, res) => {
+app.get('/attestator*', (req, res) => {
     console.log('|> request: attestator', '\n');
     res.header("Content-Type", "application/json");
     res.send(attestatorResult);
 });
-app.get('/livestock', (req, res) => {
+app.get('/livestock*', (req, res) => {
     console.log('|> request: livestock', '\n');
     res.header("Content-Type", "application/json");
     res.send(livestockResult);
