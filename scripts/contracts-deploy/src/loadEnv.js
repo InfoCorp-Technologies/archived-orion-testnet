08c26753d107b9c54913a808852c6ab1a671c2b1
Sebastián <sebaponti@gmail.com>
2019-03-25 14:47:14 -0300

    Added node scripts to deploy bridge contracts

---
+++ scripts/contracts-deploy/src/loadEnv.js
@@ -0,0 +1,54 @@
+const path = require('path')
+require('dotenv').config({
+  path: path.join(__dirname, '..', '.env')
+})
+const { isAddress, toBN } = require('web3').utils
+const envalid = require('envalid')
+
+// Validations and constants
+const bigNumValidator = envalid.makeValidator(x => toBN(x))
+const validateAddress = address => {
+  if (isAddress(address)) {
+    return address
+  }
+
+  throw new Error(`Invalid address: ${address}`)
+}
+const addressValidator = envalid.makeValidator(validateAddress)
+const addressesValidator = envalid.makeValidator(addresses => {
+  addresses.split(' ').forEach(validateAddress)
+  return addresses
+})
+
+let validations = {
+  DEPLOYMENT_ACCOUNT_PRIVATE_KEY: envalid.str(),
+  DEPLOYMENT_GAS_LIMIT: bigNumValidator(),
+  HOME_DEPLOYMENT_GAS_PRICE: bigNumValidator(),
+  FOREIGN_DEPLOYMENT_GAS_PRICE: bigNumValidator(),
+  GET_RECEIPT_INTERVAL_IN_MILLISECONDS: bigNumValidator(),
+  HOME_RPC_URL: envalid.str(),
+  HOME_BRIDGE_OWNER: addressValidator(),
+  HOME_VALIDATORS_OWNER: addressesValidator(),
+  HOME_UPGRADEABLE_ADMIN: addressValidator(),
+  HOME_DAILY_LIMIT: bigNumValidator(),
+  HOME_MAX_AMOUNT_PER_TX: bigNumValidator(),
+  HOME_MIN_AMOUNT_PER_TX: bigNumValidator(),
+  HOME_REQUIRED_BLOCK_CONFIRMATIONS: envalid.num(),
+  HOME_GAS_PRICE: bigNumValidator(),
+  FOREIGN_RPC_URL: envalid.str(),
+  FOREIGN_BRIDGE_OWNER: addressValidator(),
+  FOREIGN_VALIDATORS_OWNER: addressValidator(),
+  FOREIGN_UPGRADEABLE_ADMIN: addressValidator(),
+  FOREIGN_REQUIRED_BLOCK_CONFIRMATIONS: envalid.num(),
+  FOREIGN_GAS_PRICE: bigNumValidator(),
+  REQUIRED_NUMBER_OF_VALIDATORS: envalid.num(),
+  VALIDATORS: addressesValidator(),
+  ERC20_TOKEN_ADDRESS: addressValidator(),
+  BRIDGEABLE_TOKEN_NAME: envalid.str(),
+  BRIDGEABLE_TOKEN_SYMBOL: envalid.str(),
+  BRIDGEABLE_TOKEN_DECIMALS: envalid.num()
+}
+
+const env = envalid.cleanEnv(process.env, validations)
+
+module.exports = env
